bf9a6aa020a3ac0276ed8fc942427d3c8a960192
Jon Ringvald <jvald8@gmail.com>
2013-09-16 22:29:42 -0400

    Modifying index page and adding stylesheet

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-Welcome to P1
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project 1 - DWA15</title>
+	<link rel="stylesheet" type="text/css" href="p1.css">
+</head>
+<body>
+	<h1>Jonathan Ringvald</h1>
+	<ul>
+		<li>I'm currently a Creative Producer at <a href="http://superdigital.co">Superdigital</a></li>
+		<li>I also teach tennis if anyone is interested in a lesson ;)</li>
+		<li>A few months back I spent $59 on <a href="http://www.sublimetext.com/2">Sublime Text 2</a>, by mistake. But it's been a good editor.</li>
+		<li>A couple of months ago I tried making <a href="http://www.jonathanringvald.com">180 sites in 180 days</a>. Clearly that project is on hold.</li>
+	</ul>
+</body>
+
+</html>
